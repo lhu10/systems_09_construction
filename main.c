6a0d81123b1a6620c9bb6a6c7bafb76825061b8c
lhu10 <60020940+lhu10@users.noreply.github.com>
2020-10-27 22:26:14 -0400

    Create main.c

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "tv_show.h"
+
+int main(){
+
+    struct tv *show;
+    
+    show = new_show("Clifford the Big Red Dog", 65);
+    
+    watch(show); 
+    modify(show, "Avator: The Last AirBender", 61);
+    watch(show); 
+
+    return 0; 
+}
