0791bc97e754c8c3ce84549fa0d9d1513b8befcb
lhu10 <60020940+lhu10@users.noreply.github.com>
2020-10-27 22:31:14 -0400

    Update main.c

--- main.c
+++ main.c
@@ -9,10 +9,11 @@ int main(){
     struct tv *show;
     
     show = new_show("Clifford the Big Red Dog", 65);
-
     watch(show); 
+    
     modify(show, "Avator: The Last AirBender", 61);
     watch(show);
+    
     modify(show, "The Show You'll Watch for a Lifetime", rand()); 
     watch(show);  
 
